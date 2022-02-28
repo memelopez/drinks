0499f22dad0f7f0534c29fec52085f56290a7615
memelopez <meme30lopez@gmail.com>
2022-02-28 01:27:11 -0600

    Add html Loader

--- webpack.config.js
+++ webpack.config.js
@@ -25,8 +25,8 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        test: /\.(html)$/,
+        use: ['html-loader'],
       },
     ],
   },
